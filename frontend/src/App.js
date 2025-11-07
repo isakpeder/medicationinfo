c59d2e485fc906279a5424eaaa00e5d1c00b441b
isak <isakmpederson@gmail.com>
2025-11-06 16:51:01 -0800

    Initialize state variables, create search button event handling function, and integrate backend API and data logic into frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const [drugData, setDrugData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const searchHandler = async () => {
+    if (!searchQuery.trim() || searchQuery.trim().length < 2) {
+      setError('Plese enter at least 2 characters');
+      return;
+    }
+    setLoading(true);
+    setError(null); // Remove previous error message
+    setDrugData(null); // Hide drug data from old search
+
+    try {
+      const response = await fetch(
+        `http://localhost:5001/api/drugs/search?query=${encodeURIComponent(searchQuery)}`
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch drug data');
+      }
+
+      const data = await response.json();
+
+      if (!data.drugs || data.drugs.length === 0) {
+        setError('No drugs found');
+        return;
+      }
+
+      setDrugData(data.drugs[0]);
+    }
+
+    catch (err) {
+      console.error('Search error: ', err);
+      setError('Failed to search for drug information');
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
